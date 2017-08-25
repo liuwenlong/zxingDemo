9255613890c853fcf9dc6d9dac4f1c70e9918888
南天 <844059061@qq.com>
2017-08-25 10:39:32 +0800

    init

---
+++ app/src/main/java/demo/zxing/com/zxingdemo/MainActivity.java
@@ -0,0 +1,43 @@
+package demo.zxing.com.zxingdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView)findViewById(R.id.text);
+        registerReceiver(barCodeReceiver,new IntentFilter(CaptureActivity.ActionDecodeContent));
+    }
+
+    public void startZxing(View v){
+        startActivity(new Intent(this,CaptureActivity.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(barCodeReceiver);
+    }
+
+    BroadcastReceiver barCodeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String barCode = intent.getStringExtra(CaptureActivity.ExtraDisplayContents);
+            text.setText(barCode);
+        }
+    };
+}
