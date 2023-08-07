252405ffe0072275aa5d31bf91d46eab1db8299d
wenlong Liu <wenlongliu@voidtech.com>
2023-08-07 18:41:16 +0800

    最新版as编程成功，提交可运行apk

--- app/src/main/java/demo/zxing/com/zxingdemo/MainActivity.java
+++ app/src/main/java/demo/zxing/com/zxingdemo/MainActivity.java
@@ -4,14 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.app.Activity;
 
 import com.google.zxing.client.android.CaptureActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     TextView text;
 
